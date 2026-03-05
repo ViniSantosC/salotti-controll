27b7a424a43751a45ad2083c7329b02e5bc1a689
vinicius <vini976964150@gmail.com>
2026-03-05 09:47:20 -0300

    v3

--- controll salotti/script.js
+++
@@ -1,7 +0,0 @@
-window.alert("Bem-vindo ao meu site!");
-var nome = window.prompt("digite o nome do programador mais insano do mundo");
-window.confirm("você tem certeza que é o programador mais insano do mundo?");
-window.alert("Parabéns! Você é o programador mais insano do mundo!");
-window.alert("Só que não otario caiu na amardinha o verdadeiro melhor é " + nome);
-
-window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
