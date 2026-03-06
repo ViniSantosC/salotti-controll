229f8be72905d2d724bd87d4fbc0930623aecac0
vinicius <vini976964150@gmail.com>
2026-03-06 10:07:52 -0300

    v6

--- controll salotti/script.js
+++ controll salotti/script.js
@@ -1,3 +1,19 @@
 function sair () {
     window.location.href='login.html';
 }
+
+const form = document.getElementById("form")
+
+function renderLogin() {
+    form.innerHTML = `
+        <head>title
+        </head>
+        <img src="img/icons8-lock.svg" alt="logo" class="logo1">
+            <h2 class="tilo">Salotti-Controll</h2>
+            <h4 class="subtitulo">Controle de Alunos</h4>
+            <input type="text" placeholder="Usuário" class="inp"><br>
+            <input type="email" placeholder="Email" class="inp"><br>
+            <input type="password" placeholder="Senha" class="inp1"><br>
+            <button type="button" class="btn1" onclick="if(confirm('Agradecemos por confiar em nosso site! Seus dados serão guardados com total segurança e sigilo em nossa base')) sair()">Cadastrar</button>
+    `
+}
